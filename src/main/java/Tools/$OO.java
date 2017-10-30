5a70a5a054c2a7bbe209c02a9d2eef3a8386bbd6
yodasnow <oliverode@gmail.com>
2017-10-30 11:20:31 -0400

    Tools

---
+++ src/main/java/Tools/$OO.java
@@ -0,0 +1,20 @@
+package Tools;
+
+/**
+ * This class is my tools class where I will make
+ * and demonstrate various skills
+ * @author oliver.odendaal
+ */
+public class $OO {
+    public static void main(String[] args) {
+
+    } //End main
+
+    public static void println(String str){
+        System.out.println(str);
+    }
+    public static void print(String str){
+        System.out.print(str);
+    }
+
+} //End class
